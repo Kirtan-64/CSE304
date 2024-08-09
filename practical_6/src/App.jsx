e95fdd173f35c381cf511454fe29d89bafbd7c62
Kirtan-64 <143480445+Kirtan-64@users.noreply.github.com>
2024-08-09 04:24:22 +0000

    tmp commit

--- practical_6/src/App.jsx
+++ practical_6/src/App.jsx
@@ -1,23 +1,53 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import ListItem from './ListItem';
 
 function App() {
-  const [list, setList] = useState([]);
+  const [list, setList] = useState([{id: 1, text: 'Learn React'}, {id: 2, text: 'Learn Vue'}]);
 
-  setList([{ id: 1, title: 'Task 1', isCompleted: false }]);
+  const onDelete = (id) => {
+    // delete the item from the list
+    const newList = list.filter(item => item.id !== id);
+    setList(newList);
+
+    // update the local storage
+    localStorage.setItem('list', JSON.stringify(newList));
+  }
+
+  const onAdd = (text) => {
+    // add the item to the list
+    const newList = [...list, {id: list.length + 1, text}];
+    setList(newList);
+
+    // update the local storage
+    localStorage.setItem('list', JSON.stringify(newList));
+  }
+
+
+
+  localStorage.setItem('list', JSON.stringify(list));
+
+  useEffect(() => {
+    // fetch the list from the local storage
+    const list = localStorage.getItem('list');
+    if (list) {
+      setList(JSON.parse(list));
+    }
+  }, [])
 
   return (
     <>
       <div className="p-8 bg-violet-500">
-        <div className="p-12 bg-blue-900 flex flex-col">
-          <div>
-            <h1 className='text-xl text-white'>Get Things Done!</h1>
+        <div className="p-12 bg-blue-900 flex flex-col justify-center w-full">
+          <div className="flex justify-center w-full">
+            <h1 className='text-xl text-white p-5'>Get Things Done!</h1>
           </div>
-          <div>
-            <input type='text' />
+          <div className='flex flex-row justify-center w-full'>
+            <input type='text' className='p-2' placeholder='Add a new task' />
+            <button className='p-2 bg-violet-500 text-white' onClick={() => onAdd('Learn Angular')}>Add Task</button>
           </div>
-          <div className='p-2 felx flex-col'>
+          <div className='p-2 felx flex-col justify-center w-full'>
             {/* here will come the list component */}
-
+            <ListItem item='Learn React' />
           </div>
         </div>
       </div>
--- practical_6/src/ListItem.jsx
+++ practical_6/src/ListItem.jsx
@@ -1,6 +1,15 @@
-cont ListItem = ({props}) => {
+const ListItem = ({item}) => {
     return (
-        <h1>Hello, {props.item}</h1>
+        <div className="p-2 flex flex-row space-between justify-center w-full justify-between">
+            <div className="text-white">{item.id}1</div>
+            <div className="text-white">{item.text}this is tmp text</div>
+            <div className="flex flex-row justify-center">
+                <div>
+                    <button className="p-2 bg-green-500 text-white">Edit</button>
+                    <button className="p-2 bg-red-500 text-white">Delete</button>
+                </div>
+            </div>
+        </div>
     )
 }
 
